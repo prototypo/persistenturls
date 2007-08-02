885cb9d08e20cc8b49040091b13a326cf6797ab8
dwood <dwood@e337d069-282c-3776-fb4d-51027d26752d>
2007-08-02 23:00:50 +0000

    Initial load of PURL 2 project directories.

---
+++ requirements/README.txt
@@ -0,0 +1,9 @@
+OCLC PURL 2 Project: Requirements
+
+To view HTML pages for the requirements:
+  1.  Run ./build.sh
+  2.  Open ./generated/index.html
+
+To edit the source:
+  1.  Copy the file template.xml into a new filename
+  2.  Uncomment tags as necessary.  Tag names are (hopefully) explanatory.  If not, call Dave.
---
+++ src/mod-admin/modules/mod-admin/org/purl/accessor/DomainAccessor.java
@@ -0,0 +1,31 @@
+package org.purl.accessor;
+
+import org.ten60.netkernel.layer1.nkf.INKFConvenienceHelper;
+import org.ten60.netkernel.layer1.nkf.INKFRequestReadOnly;
+import org.ten60.netkernel.layer1.nkf.INKFResponse;
+import org.ten60.netkernel.layer1.nkf.impl.NKFAccessorImpl;
+
+import com.ten60.netkernel.urii.aspect.StringAspect;
+
+public class DomainAccessor extends NKFAccessorImpl {
+	public DomainAccessor() {
+		super(SAFE_FOR_CONCURRENT_USE, INKFRequestReadOnly.RQT_SOURCE);
+
+	}
+
+	@Override
+	public void processRequest(INKFConvenienceHelper context) throws Exception {
+		String path=context.getThisRequest().getArgument("path");
+		INKFResponse resp = null;
+
+		String[] parts=path.split("/");
+		for(int i = 0; i < parts.length; i++) {
+			System.out.println(parts[i]);
+		}
+
+		resp = context.createResponseFrom(new StringAspect("<hello>domain</hello>"));
+		resp.setMimeType("text/xml");
+		context.setResponse(resp);
+	}
+
+}
