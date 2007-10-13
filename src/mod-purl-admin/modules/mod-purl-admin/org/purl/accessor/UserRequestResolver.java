ac97ef4dfab614255ce78e2b5efbe2a51f62abca
brian <brian@e337d069-282c-3776-fb4d-51027d26752d>
2007-10-13 20:36:19 +0000

    Support for user-requests.

---
+++ src/mod-purl-admin/modules/mod-purl-admin/org/purl/accessor/UserRequestResolver.java
@@ -0,0 +1,25 @@
+package org.purl.accessor;
+
+import org.ten60.netkernel.layer1.nkf.INKFConvenienceHelper;
+import org.ten60.netkernel.layer1.nkf.NKFException;
+
+public class UserRequestResolver extends URIResolver {
+
+    @Override
+    public String getURI(INKFConvenienceHelper context) {
+        String retValue = null;
+        try {
+            retValue = getURI(NKHelper.getLastSegment(context));
+        } catch(NKFException nfe) {
+            nfe.printStackTrace();
+        }
+
+        return retValue;
+    }
+
+    @Override
+    public String getURI(String id) {
+        return "ffcpl:/user-requests/" + id;
+    }
+
+}
