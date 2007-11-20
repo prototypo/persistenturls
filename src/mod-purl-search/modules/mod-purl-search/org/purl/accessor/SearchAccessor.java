7e8bcd5d2b910a4bcd4af7a13ae95960a1254a7b
brian <brian@e337d069-282c-3776-fb4d-51027d26752d>
2007-11-20 06:46:41 +0000

    Updates to the PURL Search capabilities.

--- src/mod-purl-search/modules/mod-purl-search/org/purl/accessor/SearchAccessor.java
+++ src/mod-purl-search/modules/mod-purl-search/org/purl/accessor/SearchAccessor.java
@@ -11,6 +11,7 @@ import com.ten60.netkernel.urii.IURRepresentation;
 import com.ten60.netkernel.urii.aspect.StringAspect;
 
 public class SearchAccessor extends NKFAccessorImpl {
+    
     public SearchAccessor() {
         super(SAFE_FOR_CONCURRENT_USE, INKFRequestReadOnly.RQT_SOURCE);
     }
@@ -20,15 +21,25 @@ public class SearchAccessor extends NKFAccessorImpl {
         
         INKFRequest req = context.createSubRequest("active:luceneSearch");
         String index=context.getThisRequest().getArgument("index");
-        IAspectXDA queryXDA= (IAspectXDA) context.sourceAspect("this:param:query", IAspectXDA.class);
-        String query = queryXDA.getXDA().getText("/query", true);
-        System.out.println("QUERY: " + query);
+        //IAspectXDA queryXDA= (IAspectXDA) context.sourceAspect("this:param:query", IAspectXDA.class);
+        //String query = queryXDA.getXDA().getText("/query", true);
         IURRepresentation retValue = null;
+        int kwidx = 0;
+        String kwname = getKeywordName(kwidx);
         
         StringBuffer sb = new StringBuffer("<luceneSearch><index>");
         sb.append(index);
         sb.append("</index><query>");
-        sb.append(query);
+        
+        // TODO: Use a static list of names 'keyword0', 'keyword1', etc.
+        while(context.getThisRequest().argumentExists(kwname)) {
+            if(kwidx > 0) {
+                sb.append(" ");
+            }
+            sb.append(context.getThisRequest().getArgument(kwname).substring(8));
+            kwname=getKeywordName(++kwidx);
+        }
+        //sb.append(query);
         sb.append("</query>");
         sb.append("</luceneSearch>");
         
@@ -39,6 +50,12 @@ public class SearchAccessor extends NKFAccessorImpl {
         INKFResponse resp = context.createResponseFrom(retValue);
         resp.setMimeType("text/xml");
         context.setResponse(resp);
+    }
+    
+    private String getKeywordName(int kwidx) {
+        // TODO: Optimize this to avoid object creation for, say up to ten keywords
+        // and then return dynamically generated names for pathological cases.
+        return "keyword" + kwidx;
     }
 
 }
