211c45768cc7ccff0b602f72fe31b4218be35b55
dwood <dwood@e337d069-282c-3776-fb4d-51027d26752d>
2007-12-07 05:21:36 +0000

    Added additional pane to ask about setting <allowUserAutoCreation>.

--- src/installer/mkinstaller.sh
+++ src/installer/mkinstaller.sh
@@ -7,6 +7,7 @@ IZPACK=/Applications/IzPack
 # PURL server version number.
 VERSION=1.0b2
 
-echo 'REMINDER: Run ant all and ant deploy before this script!'
-
 $IZPACK/bin/compile IzPackInstall.xml -b . -o PURLZ-Server-$VERSION.jar -k standard
+
+echo 'REMINDER: Did you run "ant all" and "ant deploy" before this script?'
+
