8cb2b579c352897393b48e8c235cc696fe926c44
davidf <davidf@e337d069-282c-3776-fb4d-51027d26752d>
2009-01-27 21:07:01 +0000

    added test for userid case sensitivity

--- src/test/org/purl/test/ModifyUserTest.java
+++ src/test/org/purl/test/ModifyUserTest.java
@@ -59,4 +59,29 @@ public class ModifyUserTest extends AbstractIntegrationTest {
             reportException("Failed to resolve URL: ", e);
         }
     }
+            // Test modifying an existing user via an HTTP PUT.
+    public void testModifyUserUpperCaseUserName() {
+
+        try {
+            String url = "http://" + host + ":" + port + "/admin/user/TESTUSER";
+
+            Map<String, String> formParameters = new HashMap<String, String>();
+            formParameters.put("name", "Test User Modified");
+            formParameters.put("affiliation", "Zepheira, LLC");
+            formParameters.put("email", "tuser@example.com");
+            formParameters.put("passwd", "Testing!");
+            formParameters.put("hint", "We are still testing.");
+            formParameters.put("justification", "Because we like unit tests.");
+
+            String errMsg = "Cannot modify a User: ";
+            String control = "Updated resource: TESTUSER";
+            String test = client.modifyUser(url, formParameters);
+
+            // Textual response, so use assertEquals.
+            assertEquals(errMsg + test, control, test);
+
+        } catch (Exception e) {
+            reportException("Failed to resolve URL: ", e);
+        }
+    }
 }
