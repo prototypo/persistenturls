49cb9877947f69070a31244fdc38da0bdc4e97bd
ryanlee <ryanlee@e337d069-282c-3776-fb4d-51027d26752d>
2010-05-06 09:55:08 +0000

    adding diverted, filling out purl, but edit, delete, and view aren't all there

--- branches/2.0/webapps/admin/purl-form.js
+++ branches/2.0/webapps/admin/purl-form.js
@@ -1,8 +1,23 @@
+var valids = ['simple', 'renamed', 'redirect', 'described', 'copy'];
+
 var initializeForm = function() {
     var frag = document.location.hash.substr(1);
     if (frag.length === 0) frag = "simple";
+    $('#purl-form td.value').each(function() {
+        $('input', this).val($(this).attr('about'));
+
+        // get form typeof, set frag accordingly
+
+        // if form typeof is purl:PURL, look through renamed, redirect, simple, copy, described for values - any one
+        // with a value, set as frag
+    });
     modifyForm(frag);
     $('#purl-type').val(frag);
+    $('#purl-form').bind('submit', function() {
+        $('#purl-form tr:hidden td[rel] input').each(function() {
+            $(this).attr('about', null);
+        });
+    });
 };
 
 var modifyForm = function(type) {
