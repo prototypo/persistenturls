1408db3dfdde68f90afb783ca442c86dcdbcdea3
ryanlee <ryanlee@e337d069-282c-3776-fb4d-51027d26752d>
2010-05-19 02:55:55 +0000

    graph structure changes for PURL creation; returning error, but form should be correct

--- branches/2.0/webapps/admin/purl-form.js
+++ branches/2.0/webapps/admin/purl-form.js
@@ -1,28 +1,43 @@
-var valids = ['simple', 'renamed', 'redirect', 'described', 'copy'];
+var modifyForm = function(relType) {
+    $('#purl-form tr.rel').hide();
+    $('#purl-form tr.rel.'+relType).show();
+};
+
+var modifyType = function(type) {
+    $('#purl-form').attr('typeof', type);
+};
 
 var initializeForm = function() {
-    var frag = $("input[rel]").attr("rel");
+    var frag = document.location.hash.substr(1);
     if (frag.length === 0) frag = "simple";
     $('#purl-form td.value').each(function() {
-
+        $('input', this).val($(this).attr('about'));
         // get form typeof, set frag accordingly
 
         // if form typeof is purl:PURL, look through renamed, redirect, simple, copy, described for values - any one
         // with a value, set as frag
     });
     modifyForm(frag);
-    $('#purl-type').val(frag);
+    $('#purl-rel-type').val(frag);
+    $('#purl-rel-type').bind('change', function() {
+        modifyForm($(this).val());
+    });
+    $('#purl-form input[name=m_purl_type]').bind('change', function() {
+        modifyType($(this).val());
+        console.log($(this));
+        if ($(this).hasClass('partial')) {
+            $('#purl-form tr.partial').show();
+        } else {
+            $('#purl-form tr.partial').hide();
+        }
+    })
+    $('input[name=m_purl_type]:checked').trigger('change');
     $('#purl-form').bind('submit', function() {
         $('#purl-form tr:hidden td[rel] input').each(function() {
-            $(this).attr('about', null);
+            $(this).removeAttr('about');
+            $(this).val('');
         });
     });
-};
-
-var modifyForm = function(type) {
-    $("input[rel]").attr("rel", type);
-    if ($('#purl-form input.type').length > 0)
-        $('#purl-form').attr('typeof', $('#purl-form input.type.'+type).val());
 };
 
 $(document).ready(function() {
